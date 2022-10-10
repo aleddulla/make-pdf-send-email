6d19c7cf2b2835e9021ebdc9ab70bb3b116f9292
Srikanth Reddy A <sreekanthreddy.com@gmail.com>
2022-10-10 19:30:58 +0400

    Add files via upload

--- make-pdf-backend/src/main/java/com/example/demo/services/FilesStorageService.java
+++ make-pdf-backend/src/main/java/com/example/demo/services/FilesStorageService.java
@@ -1,1 +1,13 @@
+package com.example.demo.services;
 
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FilesStorageService {
+	public void init();
+
+    public void deleteAll();
+
+	public void save(MultipartFile file);
+	
+	public void saveFile(MultipartFile file, String fileName);
+}
