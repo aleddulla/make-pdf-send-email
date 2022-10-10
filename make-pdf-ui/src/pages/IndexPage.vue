adb60104243286480134083b7b7ee1f2ad5e3057
Srikanth Reddy A <sreekanthreddy.com@gmail.com>
2022-10-10 19:47:27 +0400

    Add files via upload

---
+++ make-pdf-ui/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="items-start header-text"><h5>Export PDF Generation</h5></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped>
+.q-layout {
+  min-height: 0px !important;
+}
+
+.header-text > h5 {
+  margin: 0px;
+}
+</style>
